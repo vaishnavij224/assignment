9809fd35e4f8ea9036bc3566dc089b2ad872de31
vaishnavij224 <vaishnavijagdale7654@gmail.com>
2024-05-18 18:54:20 +0530

    added

---
+++ Add.java
@@ -0,0 +1,11 @@
+public class Addition {
+	int add(int a,int b)
+	{
+		int sum=a+b;
+		return sum;
+	}
+	public static void main(String[] args) {
+		Add s=new Add();
+		System.out.println("Sum is "+add(78, 80));
+	}
+}
